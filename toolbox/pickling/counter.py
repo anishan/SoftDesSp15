988269aeefac5d3bdda53a4a43c48c4af082903a
anishan <anisha.nakagawa@students.olin.edu>
2015-03-10 00:37:37 -0400

    submitting pickling toolbox

--- toolbox/pickling/counter.py
+++ toolbox/pickling/counter.py
@@ -1,39 +1,51 @@
 """ A program that stores and updates a counter using a Python pickle file"""
 
 from os.path import exists
+import os
 import sys
 from pickle import dump, load
 
 def update_counter(file_name, reset=False):
-	""" Updates a counter stored in the file 'file_name'
+    """ Updates a counter stored in the file 'file_name'
 
-		A new counter will be created and initialized to 1 if none exists or if
-		the reset flag is True.
+        A new counter will be created and initialized to 1 if none exists or if
+        the reset flag is True.
 
-		If the counter already exists and reset is False, the counter's value will
-		be incremented.
+        If the counter already exists and reset is False, the counter's value will
+        be incremented.
 
-		file_name: the file that stores the counter to be incremented.  If the file
-				   doesn't exist, a counter is created and initialized to 1.
-		reset: True if the counter in the file should be rest.
-		returns: the new counter value
+        file_name: the file that stores the counter to be incremented.  If the file
+                           doesn't exist, a counter is created and initialized to 1.
+        reset: True if the counter in the file should be rest.
+        returns: the new counter value
 
-	>>> update_counter('blah.txt',True)
-	1
-	>>> update_counter('blah.txt')
-	2
-	>>> update_counter('blah2.txt',True)
-	1
-	>>> update_counter('blah.txt')
-	3
-	>>> update_counter('blah2.txt')
-	2
-	"""
-	pass
+    >>> update_counter('blah.txt',True)
+    1
+    >>> update_counter('blah.txt')
+    2
+    >>> update_counter('blah2.txt',True)
+    1
+    >>> update_counter('blah.txt')
+    3
+    >>> update_counter('blah2.txt')
+    2
+    """
+    if exists(file_name):
+        f = open(file_name, 'r+')
+        f.close()
+    if reset:
+        counter = 1
+    else:
+        counter = load(open(file_name, "rb"))
+        counter += 1
+    dump(counter, open(file_name, 'w'))
+    return counter
+        
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		import doctest
-		doctest.testmod()
-	else:
-		print "new value is " + str(update_counter(sys.argv[1]))
+    if len(sys.argv) < 2:
+        import doctest
+        doctest.testmod()
+    else:
+        print "new value is " + str(update_counter(sys.argv[1]))
+
