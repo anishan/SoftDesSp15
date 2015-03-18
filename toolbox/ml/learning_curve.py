d9600a57a5266a632da65c15cddba045b2c2cba2
anishan <anisha.nakagawa@students.olin.edu>
2015-03-18 19:06:45 -0400

    submit machine learning toolbox

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -12,13 +12,24 @@ num_trials = 10
 train_percentages = range(5,95,5)
 test_accuracies = numpy.zeros(len(train_percentages))
 
-# train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
-# the resultant accuracy.
-# You should repeat each training percentage num_trials times to smooth out variability
-# for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
+# train a model with training percentages between 5 and 90 (see
+# train_percentages) and evaluate the resultant accuracy.
+# You should repeat each training percentage num_trials times to smooth out
+# variability for consistency with the previous example.
+# Use model = LogisticRegression(C=10**-10) for your learner
 
-# TODO: your code here
-
+for i in range(len(train_percentages)):
+    percent = train_percentages[i] / 100.0
+    # To find the average test accuracy over a number of trials
+    for j in range(num_trials):
+        array = train_test_split(data.data, data.target, train_size = percent)
+        X_train, X_test, y_train, y_test = array
+        model = LogisticRegression(C=10**-10)
+        model.fit(X_train, y_train)
+        test_accuracies[i] += model.score(X_test,y_test)
+    test_accuracies[i] /= float(num_trials)
+    
+# Plot results
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
 plt.xlabel('Percentage of Data Used for Training')
